4d3e482eaf8104d37a9e418e3e202e13abf4a5fc
kicktv <khaledtv29@gmail.com>
2019-08-23 19:49:56 +0100

    Update run.php

--- run.php
+++ run.php
@@ -3,8 +3,38 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<style  type="text/css">
+body {background: url(https://www.html-editor.tk/img/bg-pattern.png) #1f78d1;}
+input.btn1 {font-size:20px; width:45%; height:40px;  color:black;
+font-family:impact,arial,tahoma,helvetica,sans-serif;
+background-color:#fe5900;border: 1px solid; border-color:#fff;border-radius:5px;}
+@media only screen and (max-width:980px) {input.btn1{width:47%; height:36px;}}
+input.btn2 {font-size:20px; width:45%; height:40px;  color:black;
+font-family:impact,arial,tahoma,helvetica,sans-serif;
+background-color:#fe5900;border: 1px solid; border-color:#fff;border-radius:5px;}
+@media only screen and (max-width:980px) {input.btn2{width:47%;height:36px;}}
+h1 {font-family:impact, arial,tahoma ; color:#fe5900; font:bold 95%}
+@media only screen and (max-width:980px) {h1{font-size:22px; }}
+.textinput {width:90%; height:360px; outline: none;resize: none; border: 1px solid #000;border-radius:5px;}
+@media only screen and (max-width:980px) {.textinput{width:98%;height:360px;}}</style>
+<script language="JavaScript">
+function preview(){temp=document.tester.box.value,preWindow=open("","preWindow"," menubar=yes, scrollbars=yes"),
+preWindow.document.open(),
+preWindow.document.write(temp),preWindow.document.close()}</script>
+</head><body><center><h1>HTML And Script Code Tester</h1>
+<form action="test.php" target="_blank" method="POST">
+<textarea  class="textinput"   wrap="physical" name="code"></textarea><br/>
+<input class="btn1" type="button" name="submit">
+<input class="btn2" type="reset" value="Clear Text Box"></form><br/></center>
+</body></html>
+<!--
+<!DOCTYPE html><html lang="en"><head><meta charset="utf-8">
+<title>PHP Code Tester</title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 </head><body><center><form action="test.php" target="_blank" method="POST">
 <textarea rows="8" cols="80" name="code"></textarea><br>
 <input  name="submit" type="submit" />
 <input type="reset" value="Clear Text"></form>
-</center></body></html>
+</center></body></html> -->
