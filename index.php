54f6397d610d2d9607f8d6cb318919aba7126d04
kicktv <khaledtv29@gmail.com>
2019-08-23 17:21:33 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php
-echo "zoptv.heroku.com";
-?>
+<!DOCTYPE html><html><head>
+<title>my web heroku</title>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+</head><body><center>
+<h1> Welcome To My Website</h1>  
+<h2 id="host"></h2>  
+<script>
+var url = window.location.hostname;
+document.getElementById("host").innerHTML=(url);
+</script>
+</center></body></html>
+
+
