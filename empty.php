3dab3da9a91b46484070b83dcf113fdbccbcdac9
kicktv <khaledtv29@gmail.com>
2019-08-29 00:15:05 +0100

    Update empty.php

--- empty.php
+++ empty.php
@@ -1,5 +1,5 @@
 <script language="JavaScript">
-function preview(){temp=document.tester.code.value,preWindow=open("/?","preWindow"," menubar=yes, scrollbars=yes"),
+function preview(){temp=document.tester.code.value,preWindow=open("?","preWindow"," menubar=yes, scrollbars=yes"),
 preWindow.document.open(), preWindow.document.write(temp),preWindow.document.close()}
 </script>
 
