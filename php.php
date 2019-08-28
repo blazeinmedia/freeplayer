95b688a213533cfce371647f8639ce4f9e3d11da
kicktv <khaledtv29@gmail.com>
2019-08-28 23:48:41 +0100

    Update php.php

--- php.php
+++ php.php
@@ -1,19 +1,14 @@
 <script language="JavaScript">
- function preview(){temp=document.tester.code.value,preWindow=open("","preWindow"," menubar=yes, scrollbars=yes"), preWindow.document.open(), preWindow.document.write(temp),preWindow.document.close()}
+function preview(){temp=document.tester.code.value,preWindow=open("","preWindow"," menubar=yes, scrollbars=yes"),
+preWindow.document.open(), preWindow.document.write(temp),preWindow.document.close()}
 </script>
-
-
-
 <center>
 <h1>PHP And HTML & JavaScript Code Tester</h1> 
 <form action="" name="tester" method="POST"> 
-<textarea class="textinput" wrap="physical" name="code">
+<textarea rows="8" cols="70" class="textinput" wrap="physical" name="code">
 </textarea><br/> 
 <input class="btn1" name="submit" type="submit" onclick="preview();" value="Test PHP&HTML Code"/>
 <input class="btn2" type="reset" value="Clear Text Box">
-
 <?php
  if (isset($_POST['submit'])) {$code = $_POST['code'];} eval("?> $code <?php "); ?> 
-
-
-</form><br/></center>
+</form></center>
