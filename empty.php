aad5940b051f9e22fec533c0cf39c3777d3bdd46
kicktv <khaledtv29@gmail.com>
2019-08-29 00:12:54 +0100

    Update empty.php

--- empty.php
+++ empty.php
@@ -1,1 +1,13 @@
-empty
+<script language="JavaScript">
+function preview(){temp=document.tester.code.value,preWindow=open("/?","preWindow"," menubar=yes, scrollbars=yes"),
+preWindow.document.open(), preWindow.document.write(temp),preWindow.document.close()}
+</script>
+
+<center>
+<h1>PHP And HTML & JavaScript Code Tester</h1> 
+<form action="" name="tester" method="POST"> 
+<textarea rows="8" cols="70" name="code">
+...
+</textarea><br/> 
+<input class="btn1" name="submit" type="submit" onclick="preview();" value="Test PHP&HTML Code"/>
+</form></center>
