e7f6e22731d67647009d77dc1794f00130bb77e4
kicktv <khaledtv29@gmail.com>
2019-10-08 02:47:26 +0100

    Update hls.php

--- hls.php
+++ hls.php
@@ -1,5 +1,5 @@
 <?php
-$file = "http://82.137.248.16:1935/SEdu/stream20112018/chunklist_w536453290.m3u8";
+$file = "" .$_GET['src'];
 header("Content-type: application/x-mpegURL");
 readfile($file);
 ?>
