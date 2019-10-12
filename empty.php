35e20dba950b4dd566343729c4c53f0da10b238d
kicktv <khaledtv29@gmail.com>
2019-10-12 23:03:25 +0200

    Update empty.php

--- empty.php
+++ empty.php
@@ -1,7 +1,5 @@
 <?php
-header("Content-type: application/x-mpegURL");
-
-$hls = "http://cdnamd-hls-globecast.akamaized.net/live/ramdisk/2m_monde/hls_video_ts/index.m3u8";
-readfile($hls);
+header("Location: http://cdnamd-hls-globecast.akamaized.net/live/ramdisk/2m_monde/hls_video_ts/index.m3u8");
+exit();
 ?>
 
