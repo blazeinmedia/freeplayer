4b6d3410862711d656848306c6e5d6e5e2875192
kicktv <khaledtv29@gmail.com>
2019-10-14 00:45:39 +0200

    Update drive.php

--- drive.php
+++ drive.php
@@ -1,7 +1,7 @@
 <?php
-header('Content-type: video/mp4');
 $id= $_GET['id'];
 $file = "https://www.googleapis.com/drive/v3/files/" .$id. "?alt=media&key=AIzaSyAgJNu0Gt0vAFVCN3g0fqlRrksIrscRiLM";
+header('Content-Type: application/octet-stream');
 readfile($file);
 exit;
 ?>
