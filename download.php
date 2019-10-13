3f33c15c71d5212c7c65c1fc73746c24bb096ee5
kicktv <khaledtv29@gmail.com>
2019-10-14 00:27:51 +0200

    Update download.php

--- download.php
+++ download.php
@@ -1,6 +1,24 @@
 <?php
+// You can used any file extension to output the file for download
+$file = $_GET['src'];
+
+if (file_exists($file)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="'.basename($file).'"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    readfile($file);
+    exit;
+}
+?>
+
+
+/* <?php
 header('Content-type: video/mp4');
 $file = $_GET['src'];
 readfile($file);
 exit;
-?>
+?>*/
