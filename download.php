013f357b17b92dc75ac69e21357d87af2f84c159
kicktv <khaledtv29@gmail.com>
2019-10-14 00:39:05 +0200

    Update download.php

--- download.php
+++ download.php
@@ -1,12 +1,7 @@
 <?php
-// You can used any file extension to output the file for download
 $id = "" .$_GET['id'];
 $file = "https://freeplayer.herokuapp.com/mediafire.php?id=".$id;
-
-    header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename="'.basename($file).'"');
-    header('Pragma: public');
-    header('Content-Length: ' . filesize($file));
-    readfile($file);
-    exit;
+header('Content-Type: application/octet-stream');
+readfile($file);
+exit;
 ?>
