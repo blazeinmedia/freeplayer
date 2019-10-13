ba719c61516b62daec81398b47ffd7b7452b9cea
kicktv <khaledtv29@gmail.com>
2019-10-14 00:18:44 +0200

    Update dropbox.php

--- dropbox.php
+++ dropbox.php
@@ -1,10 +1,9 @@
 <?php
-header('Content-type: video/mp4');
-
 $id= "" .$_GET['id'];
 $title = file_get_contents('https://www.dropbox.com/s/'.$id);
 preg_match('/<title>Dropbox -(.*) - Simplify/', $title, $matches);
 $title = urldecode($matches[1]);
 $link = 'https://dl.dropboxusercontent.com/s/'.$id.'/'.$title;
+header('Content-type: application/octet-stream');
 readfile($link);
 ?>
