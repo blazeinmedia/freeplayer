06020147baf37dd0e89d026edb72daba4e1b9493
kicktv <khaledtv29@gmail.com>
2019-10-14 00:02:37 +0200

    Update mediafire.php

--- mediafire.php
+++ mediafire.php
@@ -1,10 +1,10 @@
 <?php
-header('Content-type: video/mp4');
-
-$url = "" .$_GET['src'];
+$id = "" .$_GET['id'];
+$url = "https://www.mediafire.com/file/".$id;
 $link = file_get_contents($url);
 $start = explode('href="https://download',$link);
 $end = explode('">',$start[1]);
 $file = "https://download".$end[0]; 
+header('Content-type: video/mp4');
 readfile($file);
 ?>
