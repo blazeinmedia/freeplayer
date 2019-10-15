50e7104824d7578cf8824a3468f9d8148108127d
kicktv <khaledtv29@gmail.com>
2019-10-15 14:23:07 +0100

    Update hls.php

--- hls.php
+++ hls.php
@@ -8,13 +8,13 @@ die();
 }
 session_start();
 $sid = session_id();
-$path = "2m.m3u8";
+$path = "http://freeplayer.herokuapp.com/2m.m3u8";
 $hash = md5($path.$sid); //You need to use proper encryption. This is not secure at all.
 $_SESSION[$hash] = $path;
 ?>
 <!DOCTYPE html><html lang="en"><head><meta charset="utf-8">
 <title>encrypt url video with php hash</title>
-</head><body><center> <h2> encrypt url video hls with php hash</h2><br>
+</head><body><center> <h2> encrypt url video hls</h2><br>
 <video width="704" height="404" controls>
 <source src="encrypt.php?video=<?= $hash;?>" type="application/x-mpegURL">
 </video></center></body></html>
