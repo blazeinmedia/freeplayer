9dd6e5c84f8d37ba979275d79b0b947915ac6a4d
kicktv <khaledtv29@gmail.com>
2019-10-15 23:59:31 +0200

    Update download.php

--- download.php
+++ download.php
@@ -1,6 +1,7 @@
 <?php
 $file = "" .$_GET['src'];
-header('Content-Type: application/octet-stream');
+//header('Content-Type: application/octet-stream');
+header('Content-Disposition:filename="video.mp4"');
 readfile($file);
 exit;
 ?>
