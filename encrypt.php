4c524c3f3a446e03ebd3b4611d6e8b14d4873c72
kicktv <khaledtv29@gmail.com>
2019-10-16 23:23:14 +0200

    Update encrypt.php

--- encrypt.php
+++ encrypt.php
@@ -6,4 +6,9 @@ $file = $_SESSION[$_GET["video"]]; //Get the filename
 readfile($file); //Proxy
 //die();       
 }
+session_start();
+$sid = session_id();
+$path = 'tamazight.m3u8';
+$hash = md5($path.$sid); //You need to use proper encryption. This is not secure at all.
+$_SESSION[$hash] = $path;
 ?>
