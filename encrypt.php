b619cd881f8912fa71585076c4a0189a66a5526a
kicktv <khaledtv29@gmail.com>
2019-10-16 23:29:04 +0200

    Update encrypt.php

--- encrypt.php
+++ encrypt.php
@@ -1,14 +1,15 @@
 <?php
 session_start();
 if (isset($_GET["video"]) && isset($_SESSION[$_GET["video"]])) {
-header("Content-Type: application/vnd.apple.mpegurl");
+//header("Content-Type: application/vnd.apple.mpegurl");
+header('Content-Type: application/octet-stream');
 $file = $_SESSION[$_GET["video"]]; //Get the filename
 readfile($file); //Proxy
 //die();       
 }
 session_start();
 $sid = session_id();
-$path = 'tamazight.m3u8';
+$path = 'http://www.html-editor.tk/nature.mp4';
 $hash = md5($path.$sid); //You need to use proper encryption. This is not secure at all.
 $_SESSION[$hash] = $path;
 ?>
