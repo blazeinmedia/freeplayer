5f73c22398643752f8e01e7a7ba39d1d33f00f2f
kicktv <khaledtv29@gmail.com>
2019-10-16 23:04:55 +0200

    Update hls.php

--- hls.php
+++ hls.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 $sid = session_id();
-$path = '2m.m3u8';
+$path = 'http://www.html-editor.tk/tamazight.m3u8';
 $hash = md5($path.$sid); //You need to use proper encryption. This is not secure at all.
 $_SESSION[$hash] = $path;
 ?>
