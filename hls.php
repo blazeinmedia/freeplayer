b4bfe52108bd340550da05f3a58c1c498a4f5afb
kicktv <khaledtv29@gmail.com>
2019-10-16 10:36:51 +0100

    Update hls.php

--- hls.php
+++ hls.php
@@ -3,7 +3,7 @@ session_start();
 if (isset($_GET["video"]) && isset($_SESSION[$_GET["video"]])) {
 header('Content-Type: application/vnd.apple.mpegurl');
 $file = $_SESSION[$_GET["video"]]; //Get the filename
-@readfile($file); //Proxy
+readfile($file); //Proxy
 die();       
 }
 session_start();
@@ -14,7 +14,7 @@ $_SESSION[$hash] = $path;
 ?>
 <!DOCTYPE html><html lang="en"><head><meta charset="utf-8">
 <title>encrypt url video with php hash</title>
-</head><body><center> <h2> encrypt video hls</h2><br>
+</head><body><center> <h2> test encrypt video hls</h2><br>
 <video width="704" height="404" controls>
 <source src="hls.php?video=<?= $hash;?>" type="video/mp4">
 </video></center></body></html>
