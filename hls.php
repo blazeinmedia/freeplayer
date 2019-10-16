617de825b066079d78e0990bceb65411c35301cc
kicktv <khaledtv29@gmail.com>
2019-10-16 11:37:45 +0100

    Update hls.php

--- hls.php
+++ hls.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+$sid = session_id();
+$path = '2m.m3u8';
+$hash = md5($path.$sid); //You need to use proper encryption. This is not secure at all.
+$_SESSION[$hash] = $path;
+?>
+
 <!DOCTYPE html><html lang="en"><head><meta charset="utf-8">
 <title>encrypt url video with php hash</title>
 </head><body><center> <h2> test encrypt video hls m3u8</h2><br>
