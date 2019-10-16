e3306eb8ec69969196ba0363e19fbd80ddbe94a9
kicktv <khaledtv29@gmail.com>
2019-10-16 23:24:19 +0200

    Update hls.php

--- hls.php
+++ hls.php
@@ -2,7 +2,7 @@
 
 <!DOCTYPE html><html lang="en"><head><meta charset="utf-8">
 <title>encrypt url video with php hash</title>
-</head><body><center> <h2> test encrypt url hls m3u8</h2><br>
+</head><body><center> <h2> test encrypt url hls jw</h2><br>
 <iframe src="jw.html?src=encrypt.php?video=<?php echo $hash;?>" frameborder="0" width="714" height="402"></iframe> 
 <!-- <video width="704" height="404" controls>
 <source src="encrypt.php?video=<?php echo $hash;?>" type="video/mp4"></video> -->
