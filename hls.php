dec623f7414cbb766468d1dffc0ae09c78ad7459
kicktv <khaledtv29@gmail.com>
2019-10-16 11:29:10 +0100

    Update hls.php

--- hls.php
+++ hls.php
@@ -1,20 +1,6 @@
-<?php
-session_start();
-if (isset($_GET["video"]) && isset($_SESSION[$_GET["video"]])) {
-header("Content-Type: application/vnd.apple.mpegurl");
-$file = $_SESSION[$_GET["video"]]; //Get the filename
-readfile($file); //Proxy
-//die();       
-}
-session_start();
-$sid = session_id();
-$path = '2m.m3u8';
-$hash = md5($path.$sid); //You need to use proper encryption. This is not secure at all.
-$_SESSION[$hash] = $path;
-?>
 <!DOCTYPE html><html lang="en"><head><meta charset="utf-8">
 <title>encrypt url video with php hash</title>
 </head><body><center> <h2> test encrypt video hls m3u8</h2><br>
 <video width="704" height="404" controls>
-<source src="hls.php?video=<?= $hash;?>" type="video/mp4">
+<source src="encrypt.php?video=<?= $hash;?>" type="video/mp4">
 </video></center></body></html>
