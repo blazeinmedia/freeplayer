18a34731f6483a7e75d607e379240c967b121b2a
kicktv <khaledtv29@gmail.com>
2019-10-16 11:28:16 +0100

    Create encrypt.php

---
+++ encrypt.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+if (isset($_GET["video"]) && isset($_SESSION[$_GET["video"]])) {
+header("Content-Type: application/vnd.apple.mpegurl");
+$file = $_SESSION[$_GET["video"]]; //Get the filename
+readfile($file); //Proxy
+//die();       
+}
+session_start();
+$sid = session_id();
+$path = '2m.m3u8';
+$hash = md5($path.$sid); //You need to use proper encryption. This is not secure at all.
+$_SESSION[$hash] = $path;
+?>
