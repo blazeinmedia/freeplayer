bbaade04bebff5df54e18abe0cb71b68a5fed449
kicktv <khaledtv29@gmail.com>
2019-10-16 23:12:40 +0200

    Update hls.php

--- hls.php
+++ hls.php
@@ -8,7 +8,7 @@ $_SESSION[$hash] = $path;
 
 <!DOCTYPE html><html lang="en"><head><meta charset="utf-8">
 <title>encrypt url video with php hash</title>
-</head><body><center> <h2> test encrypt video hls m3u8</h2><br>
+</head><body><center> <h2> test encrypt url hls m3u8</h2><br>
 <video width="704" height="404" controls>
 <source src="encrypt.php?video=<?php echo $hash;?>" type="video/mp4">
 </video></center></body></html>
