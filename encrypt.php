7968e34fcdde105be8841747de40d93b951790f1
kicktv <khaledtv29@gmail.com>
2019-10-16 11:36:35 +0100

    Update encrypt.php

--- encrypt.php
+++ encrypt.php
@@ -6,9 +6,4 @@ $file = $_SESSION[$_GET["video"]]; //Get the filename
 readfile($file); //Proxy
 //die();       
 }
-session_start();
-$sid = session_id();
-$path = '2m.m3u8';
-$hash = md5($path.$sid); //You need to use proper encryption. This is not secure at all.
-$_SESSION[$hash] = $path;
 ?>
