d25a1a6437984825b0e0823cea2eded63e9a9e7c
kicktv <khaledtv29@gmail.com>
2019-10-16 11:00:08 +0100

    Update hls.php

--- hls.php
+++ hls.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 $sid = session_id();
-$path = '2m.m3u8';
+$path = 'm3u8.phoning;
 $hash = md5($path.$sid); //You need to use proper encryption. This is not secure at all.
 $_SESSION[$hash] = $path;
 ?>
@@ -9,5 +9,5 @@ $_SESSION[$hash] = $path;
 <title>encrypt url video with php hash</title>
 </head><body><center> <h2> test encrypt video hls m3u8</h2><br>
 <video width="704" height="404" controls>
-<source src="m3u8.php?video=<?= $hash;?>" type="video/mp4">
+<source src="<?= $hash;?>" type="video/mp4">
 </video></center></body></html>
