d4e62f05c092e1a579885b5babe05bdfdd142739
kicktv <khaledtv29@gmail.com>
2019-10-17 17:47:54 +0100

    Update download.php

--- download.php
+++ download.php
@@ -3,5 +3,6 @@ session_start();
 $sid = session_id();
 $file = "" .$_GET['src'];
 header('Content-Type: application/octet-stream');
-readfile($file.$sid);
+readfile($file);
+$_SESSION[$sid] = $file;
 ?>
