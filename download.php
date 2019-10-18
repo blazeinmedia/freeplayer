bfa4ad20c2eb68ab34de7649e04bc4f41479df34
kicktv <khaledtv29@gmail.com>
2019-10-18 02:14:59 +0200

    Update download.php

--- download.php
+++ download.php
@@ -4,10 +4,10 @@ if (isset($_GET["src"]) && isset($_SESSION[$_GET["src"]])) {
 $file = $_SESSION[$_GET["src"]];
 header('Content-Type: application/octet-stream');  
 readfile($file);}
-?>
 
 //$file = $_GET['src'];
 //header('Content-Type: application/octet-stream');
 //readfile($file);
+?>
 
 
