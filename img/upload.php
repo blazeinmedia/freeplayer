33fef728a43ba7a612524c28f0e63b611d89d626
kicktv <khaledtv29@gmail.com>
2019-10-21 00:31:30 +0200

    Update upload.php

--- img/upload.php
+++ img/upload.php
@@ -1,21 +1,69 @@
-<?php
-// Load the stamp and the photo to apply the watermark to
-$stamp = imagecreatefrompng('khaledtv.png');
-$im = imagecreatefromjpeg('11.jpg');
-
-// Set the margins for the stamp and get the height/width of the stamp image
-$marge_right = 10;
-$marge_bottom = 10;
-$sx = imagesx($stamp);
-$sy = imagesy($stamp);
-
-// Copy the stamp image onto our photo using the margin offsets and the photo 
-// width to calculate positioning of the stamp. 
-imagecopy($im, $stamp, imagesx($im) - $sx - $marge_right, imagesy($im) - $sy - $marge_bottom, 0, 0, imagesx($stamp), imagesy($stamp));
-
-// Output and free memory
-header('Content-type: image/png');
-imagepng($im);
-imagedestroy($im);
-readfile($im);
+<?php 
+// Path configuration 
+$targetDir = "up/"; 
+$watermarkImagePath = 'khaledtv.png'; 
+ 
+$statusMsg = ''; 
+if(isset($_POST["submit"])){ 
+    if(!empty($_FILES["file"]["name"])){ 
+        // File upload path 
+        $fileName = basename($_FILES["file"]["name"]); 
+        $targetFilePath = $targetDir . $fileName; 
+        $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION); 
+         
+        // Allow certain file formats 
+        $allowTypes = array('jpg','png','jpeg'); 
+        if(in_array($fileType, $allowTypes)){ 
+            // Upload file to the server 
+            if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){ 
+                // Load the stamp and the photo to apply the watermark to 
+                $watermarkImg = imagecreatefrompng($watermarkImagePath); 
+                switch($fileType){ 
+                    case 'jpg': 
+                        $im = imagecreatefromjpeg($targetFilePath); 
+                        break; 
+                    case 'jpeg': 
+                        $im = imagecreatefromjpeg($targetFilePath); 
+                        break; 
+                    case 'png': 
+                        $im = imagecreatefrompng($targetFilePath); 
+                        break; 
+                    default: 
+                        $im = imagecreatefromjpeg($targetFilePath); 
+                } 
+                 
+                // Set the margins for the watermark 
+                $marge_right = 10; 
+                $marge_bottom = 10; 
+                 
+                // Get the height/width of the watermark image 
+                $sx = imagesx($watermarkImg); 
+                $sy = imagesy($watermarkImg); 
+                 
+                // Copy the watermark image onto our photo using the margin offsets and  
+                // the photo width to calculate the positioning of the watermark. 
+                imagecopy($im, $watermarkImg, imagesx($im) - $sx - $marge_right, imagesy($im) - $sy - $marge_bottom, 0, 0, imagesx($watermarkImg), imagesy($watermarkImg)); 
+                 
+                // Save image and free memory 
+                imagepng($im, $targetFilePath); 
+                imagedestroy($im); 
+     
+                if(file_exists($targetFilePath)){ 
+                    $statusMsg = "The image with watermark has been uploaded successfully."; 
+                }else{ 
+                    $statusMsg = "Image upload failed, please try again."; 
+                }  
+            }else{ 
+                $statusMsg = "Sorry, there was an error uploading your file."; 
+            } 
+        }else{ 
+            $statusMsg = 'Sorry, only JPG, JPEG, and PNG files are allowed to upload.'; 
+        } 
+    }else{ 
+        $statusMsg = 'Please select a file to upload.'; 
+    } 
+} 
+ 
+// Display status message 
+echo $statusMsg;
 ?>
