161f47c09bd031b49b5ee532eafd3151207867f0
kicktv <khaledtv29@gmail.com>
2019-10-20 22:38:38 +0200

    Update upload.php

--- img/upload.php
+++ img/upload.php
@@ -1,8 +1,7 @@
 <?php
-
 // Load the stamp and the photo to apply the watermark to
-$stamp = imagecreatefrompng(“khaledtv.png”);
-$im = imagecreatefromjpeg(“Success-Kid.jpg”);
+$stamp = imagecreatefrompng('khaledtv.png');
+$im = imagecreatefromjpeg('Success-Kid.jpg');
 
 // Set the margins for the stamp and get the height/width of the stamp image
 $marge_right = 10;
@@ -10,12 +9,12 @@ $marge_bottom = 10;
 $sx = imagesx($stamp);
 $sy = imagesy($stamp);
 
-// Copy the stamp image onto our photo using the margin offsets and the photo
-// width to calculate positioning of the stamp.
-imagecopy($im, $stamp, imagesx($im) – $sx – $marge_right, imagesy($im) – $sy – $marge_bottom, 0, 0, imagesx($stamp), imagesy($stamp));
+// Copy the stamp image onto our photo using the margin offsets and the photo 
+// width to calculate positioning of the stamp. 
+imagecopy($im, $stamp, imagesx($im) - $sx - $marge_right, imagesy($im) - $sy - $marge_bottom, 0, 0, imagesx($stamp), imagesy($stamp));
 
 // Output and free memory
-header(“Content-type: image/png”);
+header('Content-type: image/png');
 imagepng($im);
 imagedestroy($im);
 ?>
