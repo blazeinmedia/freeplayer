ba1b0d9f2c640dc11bc1e053845e64cf8b962848
kicktv <khaledtv29@gmail.com>
2019-10-22 02:24:42 +0200

    Update hls.php

--- hls/hls.php
+++ hls/hls.php
@@ -15,8 +15,8 @@ $_SESSION[$hash] = $path;
 <!DOCTYPE html><html><head><meta charset=utf-8 />
 <title>test hls video </title>
 <style type="text/css">video {object-fit: fill;}</style>
-<link href="http://bomtv.gitlab.io/live/video-js.css" rel="stylesheet">
-<script src="http://bomtv.gitlab.io/live/video.js"></script></head><body><center>
+<link href="//bomtv.gitlab.io/live/video-js.css" rel="stylesheet">
+<script src="//bomtv.gitlab.io/live/video.js"></script></head><body><center>
 <video id="video" class="video-js vjs-default-skin vjs-big-play-centered vjs-controls-enabled" poster=""
 controls autoplay preload="auto" width="714" height="402" data-setup='{}'></video></center>
 <script>const player = videojs('video');  player.src({
