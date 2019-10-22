ce3df7c5f99c08988cddd1a6c43e9f077b6b6a14
kicktv <khaledtv29@gmail.com>
2019-10-22 02:48:26 +0200

    Update hls.php

--- hls/hls.php
+++ hls/hls.php
@@ -5,9 +5,9 @@
 <link href="//bomtv.gitlab.io/live/video-js.css" rel="stylesheet">
 <script src="//bomtv.gitlab.io/live/video.js"></script></head><body><center>
 <video id="video" class="video-js vjs-default-skin vjs-big-play-centered vjs-controls-enabled" poster=""
-controls autoplay preload="auto" width="714" height="402" data-setup='{}'></video></center>
+controls autoplay preload="auto" width="714" height="402" data-setup='{}'></video>
 <script>const player = videojs('video');  player.src({
 src: 'hls.php?video=<?php echo $hash;?>',
 type: 'application/x-mpegURL'  }); </script><br><br>
-<a href="http://freeplayer.herokuapp.com/hls/embed.html?src=hls.php?video=<?php echo $hash;?>"> link hls token </a>
-</body></html>
+<a href="http://freeplayer.herokuapp.com/hls/embed.html?src=hls.php?video=<?php echo $hash;?>"><h2> link hls token </h2></a>
+</center></body></html>
