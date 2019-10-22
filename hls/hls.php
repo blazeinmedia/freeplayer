8a78b0c9722105b9755c837a4e7b90ecd0fe3f42
kicktv <khaledtv29@gmail.com>
2019-10-22 02:24:20 +0200

    Update hls.php

--- hls/hls.php
+++ hls/hls.php
@@ -12,10 +12,13 @@ $hash = md5($path.$sid); //You need to use proper encryption. This is not secure
 $_SESSION[$hash] = $path;
 ?>
 
-<!DOCTYPE html><html lang="en"><head><meta charset="utf-8">
-<title>encrypt url video with php hash</title>
-</head><body><center> <h2> test encrypt url hls jw</h2><br>
-<iframe src="/jw.html?src=/hls/hls.php?video=<?php echo $hash;?>" frameborder="0" width="714" height="402"></iframe> 
-<!-- <video width="704" height="404" controls>
-<source src="hls.php?video=<?php echo $hash;?>" type="video/mp4"></video> -->
-</center></body></html>
+<!DOCTYPE html><html><head><meta charset=utf-8 />
+<title>test hls video </title>
+<style type="text/css">video {object-fit: fill;}</style>
+<link href="http://bomtv.gitlab.io/live/video-js.css" rel="stylesheet">
+<script src="http://bomtv.gitlab.io/live/video.js"></script></head><body><center>
+<video id="video" class="video-js vjs-default-skin vjs-big-play-centered vjs-controls-enabled" poster=""
+controls autoplay preload="auto" width="714" height="402" data-setup='{}'></video></center>
+<script>const player = videojs('video');  player.src({
+src: 'hls.php?video=<?php echo $hash;?>',
+type: 'application/x-mpegURL'  }); </script></body></html>
