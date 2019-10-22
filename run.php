9ef174d543b5b57de88f9e41b20c51dd20d5996a
kicktv <khaledtv29@gmail.com>
2019-10-23 01:15:10 +0200

    Update run.php

--- run.php
+++ run.php
@@ -11,7 +11,7 @@ at the very beginning (first line):<br>&lt;?php include("'.str_replace('\\','\\\
 <div style="width:500px; margin-left:auto; margin-right:auto; text-align:center">
 <form method="post"><h3>Please enter password to access this page</h3>
 <font color="red"><?php echo $error_msg; ?></font><br />
-<?php if (USE_USERNAME) echo 'Login:<br /><input type="input" name="access_login" /><br />Password:<br />'; ?>
+<?php if (USE_USERNAME) echo 'Login:<br /><input type="email" name="access_login" /><br />Password:<br />'; ?>
 <input type="password" name="access_password" /><p></p><input type="submit" name="Submit" value="Submit" />
 </form><br /><a style="font-size:9px; color: #B0B0B0; font-family: Verdana, Arial;" 
 href="#" title="Download Password Protector">Powered by Password Protect</a></div></body></html>
