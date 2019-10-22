5da6165ad2ab5a9945281f0997a7b4558d206b15
kicktv <khaledtv29@gmail.com>
2019-10-22 02:29:30 +0200

    Update hls.php

--- hls/hls.php
+++ hls/hls.php
@@ -1,17 +1,3 @@
-<?php
-session_start();
-if (isset($_GET["video"]) && isset($_SESSION[$_GET["video"]])) {
-header("Content-Type: application/vnd.apple.mpegurl");
-$file = $_SESSION[$_GET["video"]]; //Get the filename
-readfile($file); 
-}
-session_start();
-$sid = session_id();
-$path = 'video.m3u8';
-$hash = md5($path.$sid); //You need to use proper encryption. This is not secure at all.
-$_SESSION[$hash] = $path;
-?>
-
 <!DOCTYPE html><html><head><meta charset=utf-8 />
 <title>test hls video </title>
 <style type="text/css">video {object-fit: fill;}</style>
@@ -20,5 +6,5 @@ $_SESSION[$hash] = $path;
 <video id="video" class="video-js vjs-default-skin vjs-big-play-centered vjs-controls-enabled" poster=""
 controls autoplay preload="auto" width="714" height="402" data-setup='{}'></video></center>
 <script>const player = videojs('video');  player.src({
-src: 'hls.php?video=<?php echo $hash;?>',
+src: 'encrypt.php?video=<?php echo $hash;?>',
 type: 'application/x-mpegURL'  }); </script></body></html>
