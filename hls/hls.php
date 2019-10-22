6c837412824bddbcb434a1d7f170c1467fa5a730
kicktv <khaledtv29@gmail.com>
2019-10-22 14:09:09 +0100

    Update hls.php

--- hls/hls.php
+++ hls/hls.php
@@ -9,6 +9,6 @@ controls autoplay preload="auto" width="714" height="402" data-setup='{}'></vide
 <script>const player = videojs('video');  player.src({
 src: 'hls.php?video=<?php echo $hash;?>',
 type: 'application/x-mpegURL'  }); </script><br><br>
-<video width="704" height="402" src="hls.php?video=<?php echo $hash;?>" type="video/mp4"></video><br>
+<video controls width="704" height="402" src="hls.php?video=<?php echo $hash;?>" type="video/mp4"></video><br>
 <a href="http://freeplayer.herokuapp.com/hls/hls.php?video=<?php echo $hash;?>"><h2> link hls token </h2></a>
 </center></body></html>
